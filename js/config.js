7f2e21122e94d288eeac22192b42e940b659ab64
Roman <121574326+romanroflan32152@users.noreply.github.com>
2025-12-09 19:29:36 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -3,18 +3,17 @@ const GAME_HEIGHT = 990;
 
 const phaserConfig = {
     type: Phaser.AUTO,
-    width: GAME_WIDTH,
-    height: GAME_HEIGHT,
     parent: 'game-container',
     backgroundColor: '#000000',
     scene: [PreloadScene, GameScene],
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: GAME_WIDTH,
+        height: GAME_HEIGHT,
     },
     physics: {
         default: 'arcade',
         arcade: { debug: false }
     },
-
 };
